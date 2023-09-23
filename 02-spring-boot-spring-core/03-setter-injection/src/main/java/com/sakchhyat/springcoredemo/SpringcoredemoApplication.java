c507f13830dc6cbbc3a7d3c71fe246ab616f79e7
sakchhyat <84785711+sakchhyat@users.noreply.github.com>
2023-09-23 17:19:55 -0400

    setter-injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/sakchhyat/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,17 @@
+package com.sakchhyat.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//		(
+//		scanBasePackages = {"com.sakchhyat.springcoredemo",
+//		"com.sakchhyat.util"}
+//)
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
